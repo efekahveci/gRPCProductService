de19d34e955046935ca163a16a71c2dcd1d067b0
efekahveci <efekahveci@hotmail.com>
2022-05-14 14:16:14 +0300

    Add project files.

---
+++ gRPCProductService/Repository/ProductRepository.cs
@@ -0,0 +1,31 @@
+﻿using ProductProto;
+using Bogus;
+namespace gRPCProductService.Repository;
+
+
+public class ProductRepository
+{
+    public ProductRepository()
+    {
+
+    }
+
+    public List<ProductModel> products = new List<ProductModel>();
+
+    public List<ProductModel> GetProducts()
+    {
+    
+
+
+        var productFaker = new Faker<ProductModel>()
+            .RuleFor(x => x.ProductId, x => ++ x.IndexVariable)
+            .RuleFor(x => x.ProductName, x => x.Commerce.ProductName());
+
+      var test =  productFaker.Generate(5);
+
+        return test;
+
+    }
+
+
+}
